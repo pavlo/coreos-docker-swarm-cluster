74e5ece67c6d43ccc2afbadd2d282a09bb574cab
pavlo <pavlo@cleverua.com>
2017-08-12 12:25:02 +0300

    the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-source /etc/environment
-source /etc/custom-environment
-
+echo "------------------------------"
 echo "BOOTSTRAP FOR $1 IS RUNNING"
 
-env
+set -o allexport
+source /etc/custom-environment
+set +o allexport
+
+unit_files="$1-systemd-units.txt"
+cat $unit_files | while read unit
+do
+   echo "Starting unit: $unit..."
+   cp -rf ./systemd-units/$unit /etc/systemd/system
+   #systemctl enable $unit
+   systemctl start $unit
+done
+
+echo "BOOTSTRAP FOR $1 COMPLETED"
+echo "------------------------------"
