5ab309bfdb814ac1a4a4ed07d6222228eff4eddc
pavlo <pavlo@cleverua.com>
2017-08-12 20:20:28 +0300

    a worker node leaves swarm when goes down

--- tools/node-down.sh
+++ tools/node-down.sh
@@ -8,4 +8,9 @@ cluster_config_dir=/etc/coreos-docker-swarm-cluster
 slack=$cluster_config_dir/tools/send-message-to-slack.sh
 
 /usr/bin/etcdctl rm /nodes/${NODE_ROLE}s/${COREOS_PRIVATE_IPV4}
+
+if [ "$ROLE_NODE" == "worker" ]; then
+    docker swarm leave
+fi
+
 $slack -m "_${COREOS_PRIVATE_IPV4}_ is DOWN ($NODE_ROLE)" -u $SLACK_WEBHOOK_URL -c "$SLACK_CHANNEL"
