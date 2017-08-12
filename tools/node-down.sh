582970285e11000f9eb1cb479506b904ef453bc4
pavlo <pavlo@cleverua.com>
2017-08-12 16:22:40 +0300

    un-registers the node in etcd

--- tools/node-down.sh
+++ tools/node-down.sh
@@ -7,4 +7,5 @@ set +o allexport
 cluster_config_dir=/etc/coreos-docker-swarm-cluster
 slack=$cluster_config_dir/tools/send-message-to-slack.sh
 
+/usr/bin/etcdctl rm /nodes/${NODE_ROLE}s/${COREOS_PRIVATE_IPV4}
 $slack -m "Node ${COREOS_PRIVATE_IPV4} ($NODE_ROLE) is DOWN" -u $SLACK_WEBHOOK_URL -c "$SLACK_CHANNEL"
